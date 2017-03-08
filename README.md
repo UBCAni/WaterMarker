49722fe2364b6ff80ac8d80862a5d83a34d3156d
TwelveNights <eddiezhou1996@gmail.com>
2017-03-08 00:14:55 -0800

    Add examples to repository

---
+++ README.md
@@ -0,0 +1,14 @@
+# WaterMarker
+A water marking tool
+
+# Instructions
+- Install requirements via `pip install -r requirements.txt`
+- Put images to be watermarked under `images/`
+- Run `python main.py`
+- Wait
+
+# Customization
+- Run `python main.py` with an optional `offset` argument
+    - e.g. `python main.py 100`
+- Change the file `mark.png` in the root directory to modify the watermark used
+
--- main.py
+++ main.py
@@ -1,31 +1,50 @@
+import argparse
 import os
 from PIL import Image, ImageDraw, ImageFile
 
-def watermark(position):
-    directory = "images"
-    images = []
+directory = "images/"
+scale = 2
+
+def set_mark(position):
     mark = Image.open("mark.png")
-
-    mark = mark.resize((mark.size[0] * 2, mark.size[1] * 2))
+    mark = mark.resize((mark.size[0] // scale, mark.size[1] // scale))
     mark_w, mark_h = mark.size
-    os.chdir(directory)
 
-    for filename in os.listdir():
-        if os.path.isfile(filename):
-            im = Image.open(filename).convert(mode="RGBA")
-            im_w, im_h = im.size
+    for filename in os.listdir(directory):
+        try:
+            path = directory + filename
+            if os.path.isfile(path):
+                with open(path, 'rb') as pic, open("marked/" + filename, 'w') as out:
+                        put_mark(pic, mark, position).save(out)
 
-            overlay = Image.new('RGBA', im.size, (0, 0, 0, 0))
-            overlay.paste(mark, (position, im_h - mark_h - position), mark)
-
-            im.paste(overlay, (0, 0), overlay)
-
-            images.append((filename, im))
-
-    os.chdir("marked")
-
-    for i in images:
-        i[1].save(i[0])
+        except Exception as e:
+            print(f"An unexpected error occurred\n{e}")
 
 
-watermark(100)
+def put_mark(picture, mark, position):
+    im = Image.open(picture).convert(mode="RGBA")
+    im = im.resize((im.size[0] // scale, im.size[1] // scale))
+
+    im_w, im_h = im.size
+    offset = (position, position)
+    im.paste(mark, offset, mark)
+
+    print(f"Processing {picture.name}")
+
+    return im
+
+
+def change_opacity(img, modifier):
+    img_w, img_h = img.size
+    pixels = img.load()
+    for x in range(img_w):
+        for y in range(img_h):
+            pixel = pixels[x, y]
+            pixels[x, y] = (pixel[0], pixel[1], pixel[2], 0 if pixel[3] + modifier < 0 else pixel[3] + modifier)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Watermark a batch of images')
+    parser.add_argument('offset', nargs='?', help="The offset from the corner", default=50)
+    args = parser.parse_args()
+
+    set_mark(args.offset)
